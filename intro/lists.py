1202c7e3973953b4efbc226e68562ef173e75f76
AhsanAAR <ahsanabdurrehman@gmail.com>
2023-08-27 14:47:29 +0500

    added deletion

--- intro/lists.py
+++ intro/lists.py
@@ -2,11 +2,14 @@
 array = [5, 8, 1, -9, 4, 2]
 print('original array', array, '\n')
 
-array.sort()  # O(nlogn)
+array.sort()  # O(nlogn) in-place
 print('sorted array', array, '\n')
 
 array.append(90)  # O(1) average
 print('insert at end', array, '\n')
+
+array.insert(0, -90)  # O(n)
+print('insert at front', array, '\n')
 
 popped_element = array.pop()  # O(1) average
 print('popped array', array)
@@ -14,7 +17,8 @@ print('popped_element', popped_element, '\n')
 
 print('loop over values')
 for item in array:
-    print(item+1)
+    item += 1
+    print(item)
 print('unchanged array', array, '\n')
 
 # in case we need index of item. Useful when modifying
